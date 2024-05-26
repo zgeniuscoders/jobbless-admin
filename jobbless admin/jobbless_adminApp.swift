00288fd1abb419e11efa4af37e14604c62a17004
zgeniuscoders <zgeniuscoders.com>
2024-05-26 18:45:10 +0100

    Initial Commit

---
+++ jobbless admin/jobbless_adminApp.swift
@@ -0,0 +1,17 @@
+//
+//  jobbless_adminApp.swift
+//  jobbless admin
+//
+//  Created by ZGenius on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct jobbless_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
